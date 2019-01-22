f3ede2d2ef2aab2a09568a6c231fb2f17546510f
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-01-22 11:53:01 +0700

    basic routing added

--- src/App.js
+++ src/App.js
@@ -1,54 +1,36 @@
 import React, { Component } from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+
 import "./App.css";
-import PostText from "./components/postText";
-import { CopyToClipboard } from "react-copy-to-clipboard";
-import * as firebase from "firebase";
+import Post from "./components/post";
+import PostsList from "./components/postList";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const indexRoutes = [
+  { path: "/", name: "Home", component: PostsList },
+  { path: "/posts/:postId", name: "Post edit page", component: Post }
+];
 
 class App extends Component {
-  state = {
-    text: "New post",
-    hasLoadedTextAtLeastOnce: false
-  };
-
-  handleChange = newText => {
-    this.setState({ text: newText });
-    const textRef = firebase.database().ref("react/text");
-    textRef.set(newText);
-  };
-
-  componentDidMount() {
-    const textRef = firebase.database().ref("react/text");
-    textRef.on("value", snap => {
-      this.setState({ text: snap.val(), hasLoadedTextAtLeastOnce: true });
-    });
-  }
-
   render() {
-    if (!this.state.hasLoadedTextAtLeastOnce) {
-      return <h1>Loading...</h1>;
-    }
     return (
-      <div className="App">
-        <div className="container m-1">
-          <div className="row">
-            <div className="col">
-              <img src={require("./img/photo-large.jpg")} alt="logo" />
-            </div>
-            <div className="col">
-              <PostText text={this.state.text} onChange={this.handleChange} />
-            </div>
-          </div>
-        </div>
-        <CopyToClipboard text={transformSpaces(this.state.text)}>
-          <button className="btn-primary m-1">Copy</button>
-        </CopyToClipboard>
-      </div>
+      <HashRouter>
+        <Switch>
+          {indexRoutes.map((prop, key) => {
+            return (
+              <Route
+                path={prop.path}
+                exact={true}
+                component={prop.component}
+                key={key}
+              />
+            );
+          })}
+        </Switch>
+      </HashRouter>
     );
   }
 }
 
 export default App;
-
-function transformSpaces(text) {
-  return text.replace(/\n\n/g, "\n⠀\n");
-}
